c9a57978d9bae3d58f2a85d549fc7a7ee43a7dff
Tim <realitystinks247@gmail.com>
2025-07-03 02:04:58 -0400

    tighten up UIEnableButtonOnGameOver

--- Assets/Scripts/UI/UIEnableButtonOnGameOver.cs
+++ Assets/Scripts/UI/UIEnableButtonOnGameOver.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class UIEnableButtonOnGameOver : MonoBehaviour, IOnGameOver {
-    private Button thisButton;
+    Button thisButton;
 
     void Awake() {
         thisButton = GetComponent<Button>();
     }
+
     public void OnGameOver() {
         thisButton.interactable = true;
         thisButton.image.color = new Color32(255, 255, 255, 255);
